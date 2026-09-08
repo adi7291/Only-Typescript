48fab5a325c20c3fa5112df9d27b780fa0a2b15e
Ramessh lal <laldasramesh@gmail.com>
2026-09-08 21:09:11 +0530

    learning data types

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,1 +1,5 @@
-declare const messages: string;
+declare let a: number;
+declare let b: number;
+declare let userName: string;
+declare let anotherUser: string;
+declare let isActive: boolean;
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,13 @@
 "use strict";
-const messages = "Hello World";
-console.log(messages);
+// data types
+// number
+let a = 100;
+let b = 100.02;
+console.log(a, b);
+//string
+let userName = "Ramessh lal";
+let anotherUser = "Vikassh lal";
+console.log(userName, anotherUser);
+//Boolean
+let isActive = true;
+console.log(isActive);
