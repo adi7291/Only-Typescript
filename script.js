935f48c38d41ab64d507139969231b2f951aaabe
unknown <laldasramesh@gmail.com>
2026-09-08 19:55:30 +0530

    change in file

---
+++ script.js
@@ -0,0 +1,3 @@
+"use strict";
+const messages = "Hello Ramessh lal. How are you? Are you good?";
+console.log(messages);
---
+++ script.ts
@@ -0,0 +1,2 @@
+const messages: string = "Hello Ramessh lal. How are you? Are you good";
+console.log(messages);
